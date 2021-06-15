f605731cf3b13f5eecf0ddd66c18e07d5f777104
Hanz07 <85916354+Hanz07@users.noreply.github.com>
2021-06-15 10:24:57 +0700

    Add files via upload

---
+++ Hanz07.sh
@@ -0,0 +1,90 @@
+#1/usr/bin/sh
+
+#warna
+green='\033[32;1m'
+blue='\033[34;1m'
+cyan='\033[36;1m'
+purple='\033[35;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+
+yellow='\033[33;1m'
+
+echo 
+
+read -p "[+] username :" bro
+
+clear
+
+
+sleep 1
+echo $green
+sleep 5
+echo ""
+figlet "Report Fb"
+echo 
+echo $green"«======================================================================>"
+echo $white"[+] AUTHOR: HANZ "
+echo $purple"[+] YOUTUBE : HANZ 07"
+echo $w"[+] SUBSCRIBE :https://youtube.com/channel/UCvFvu0EgfeA1bsb-nCH0G5w"
+echo $yellow"[+] DILARANG RECORD"
+echo $green"«=========================================================================»"
+
+read -p "[+] Masukan link fb :" bro
+echo 
+sleep 3
+
+
+
+
+echo 
+echo 
+echo 
+sleep 2
+echo $white"[+] Mulai Report Yang Ke 1 :Berhasil√"
+sleep 3
+echo $white"[+] Mulai Report Yang Ke 2 :Berhasil√"
+sleep 3
+echo $white"[+] Mulai Report Yang Ke 3 :Berhasil√"
+sleep 3
+echo $white"[+] Mulai Report Yang Ke 4 :Berhasil√"
+sleep 3
+echo $white"[+] Mulai Report Yang Ke 5 :Berhasil√"
+sleep 3
+echo $white"[+] Mulai Report Yang Ke 6 :Berhasil√"
+sleep 3
+echo $white"[+] Mulai Report Yang Ke 7 :Berhasil√"
+sleep 3
+echo $white"[+] Mulai Report Yang Ke 8 :Berhasil√"
+sleep 3
+echo $white"[+] Mulai Report Yang Ke 9 :Berhasil√"
+sleep 3
+echo $white"[+] Mulai Report Yang Ke 10:Berhasil√"
+Sleep 3
+echo $white"[+] Mulai Report Yang Ke 11:Berhasil√"
+sleep 3
+echo $white"[+] Mulai Report Yang Ke 12:Berhasil√"
+sleep 3
+echo $white"[+] Mulai Report Yang Ke 13:Berhasil√"
+Sleep 3
+echo $white"[+] Mulai Report Yang Ke 14 :Berhasil√"
+sleep 3
+echo $white"[+] Mulai Report Yang Ke 15 :Berhasil√"
+sleep 3
+echo $white"[+] Mulai Report Yang Ke 16 :Berhasil√"
+Sleep 3
+echo $white"[+] Mulai Report Yang Ke 17 :Berhasil√"
+sleep 3
+echo $white"[+] Mulai Report Yang Ke 18 :Berhasil√"
+sleep 3
+echo $white"[+] Mulai Report Yang Ke 19 :Berhasil√"
+Sleep 3
+echo $white"[+] Mulai Report Yang Ke 20 :Berhasil√"
+sleep 3
+echo $white"[+] Mulai Report Yang Ke 21 :Berhasil√"
+sleep 3
+echo $white"[+] Mulai Report Yang Ke 22 :Berhasil√"
+Sleep 3
+echo $white"[+] Mulai Report Yang Ke 23 :Berhasil√"
+echo 
+echo $white"limit,silahkan coba lagi!"
